911f4fc60decc46ff131189902bf2706abb60fad
sdfhrhfhgjdh38277 <153998312+sdfhrhfhgjdh38277@users.noreply.github.com>
2024-01-02 15:34:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+# imports
 import pyautogui as pg
-from time import sleep
-import pyttsx3
+import webbrowser
+from voice_py import *
 
-engine = pyttsx3.init()
-engine.setProperty("voice", "ru")
+# hello phrase and window to request
+hello()
 com = pg.prompt("Enter command (1 from set): search, open, say, help: ")
 
 # commands list:
-commands = ["search", "open", "say", "help"]
+commands = ["search", "open", "say", "help", "commands"]
 # search command
 if com == commands[0]:
+    search_command()
     user_input = pg.prompt("Enter text and browser (Example: youtube.com, chrome): ")
     search_text, browser_name = map(
         str.strip, user_input.split(",")
@@ -22,12 +24,24 @@ if com == commands[0]:
     sleep(2)
     pg.write(search_text, interval=0.5)
     pg.press("enter")
-
+    engine.say("Complete")
+    engine.runAndWait()
 # open command
 if com == commands[1]:
+    open_command()
     open_app = pg.prompt("Enter app name: ")
     sleep(1)
     pg.hotkey("winleft")
     sleep(0.5)
     pg.write(open_app, 0.5)
     pg.press("enter")
+# say command
+if com == commands[2]:
+    say_text = pg.prompt("Enter text to say")
+    engine.say(say_text)
+# all commands
+if com == commands[3]:
+    all_commands()
+
+if com == commands[4]:  # help command
+    help()
